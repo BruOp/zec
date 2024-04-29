74287475ce0b92c7e93fe91528f2707c63ab618c
Bruno Opsenica <bruno.opsenica@gmail.com>
2024-04-28 23:29:23 -0400

    Update example-02

--- examples/02-normal-mapping/02-normal-mapping.cpp
+++ examples/02-normal-mapping/02-normal-mapping.cpp
@@ -1,7 +1,9 @@
-#include "app.h"
-#include "core/zec_math.h"
-#include "utils/exceptions.h"
-#include "camera.h"
+#include <windows.h>
+#include <app.h>
+#include <core/zec_math.h>
+#include <utils/exceptions.h>
+#include <imgui/imgui.h>
+#include <camera.h>
 
 using namespace zec;
 
@@ -38,72 +40,72 @@ public:
 
     PerspectiveCamera camera = {};
     OrbitCameraController camera_controller = OrbitCameraController{ };
-    MeshHandle cube_mesh = {};
-    BufferHandle view_cb_handle = {};
-    BufferHandle draw_cb_handle = {};
-    TextureHandle albedo_map = {};
-    TextureHandle normal_map = {};
-    ResourceLayoutHandle resource_layout = {};
-    PipelineStateHandle pso_handle = {};
+    rhi::Draw cube_mesh = {};
+    rhi::BufferHandle view_cb_handle = {};
+    rhi::BufferHandle draw_cb_handle = {};
+    rhi::TextureHandle albedo_map = {};
+    rhi::TextureHandle normal_map = {};
+    rhi::ResourceLayoutHandle resource_layout = {};
+    rhi::PipelineStateHandle pso_handle = {};
     ViewConstantData view_constant_data = {};
     DrawConstantData draw_constant_data = {};
 
 protected:
     void init() override final
     {
-        camera_controller.init();
-        camera_controller.set_camera(&camera);
-
+        camera_controller.settings = {
+            .yaw_sensitivity = 200.0f,
+            .pitch_sensitivity = 200.0f,
+        };
         camera_controller.origin = vec3{ 0.0f, 0.0f, 0.0f };
         camera_controller.radius = 2.0f;
         // Create a root signature consisting of a descriptor table with a single CBV.
         {
             constexpr u32 num_textures = 1024;
-            ResourceLayoutDesc layout_desc{
+            rhi::ResourceLayoutDesc layout_desc{
                 .num_constants = 0,
                 .constant_buffers = {
-                    { ShaderVisibility::ALL },
-                    { ShaderVisibility::ALL },
+                    { rhi::ShaderVisibility::ALL },
+                    { rhi::ShaderVisibility::ALL },
                 },
                 .num_constant_buffers = 2,
-                .tables = {
-                    {.usage = ResourceAccess::READ, .count = 4096 },
-                },
-                .num_resource_tables = 1,
-                .static_samplers = {
-                    {
-                        .filtering = SamplerFilterType::ANISOTROPIC,
-                        .wrap_u = SamplerWrapMode::WRAP,
-                        .wrap_v = SamplerWrapMode::WRAP,
-                        .binding_slot = 0,
-                    },
-                },
-                .num_static_samplers = 1,
             };
 
-            resource_layout = gfx::pipelines::create_resource_layout(layout_desc);
+            resource_layout = renderer.resource_layouts_create(layout_desc);
         }
 
         // Create the Pipeline State Object
         {
-            PipelineStateObjectDesc pipeline_desc = {};
-            pipeline_desc.input_assembly_desc = { {
-                { MESH_ATTRIBUTE_POSITION, 0, BufferFormat::FLOAT_3, 0 },
-                { MESH_ATTRIBUTE_NORMAL, 0, BufferFormat::FLOAT_3, 1 },
-                { MESH_ATTRIBUTE_TEXCOORD, 0, BufferFormat::FLOAT_2, 2 },
-            } };
-            pipeline_desc.shader_file_path = L"shaders/normal_mapping.hlsl";
-            pipeline_desc.rtv_formats[0] = BufferFormat::R8G8B8A8_UNORM_SRGB;
-            pipeline_desc.resource_layout = resource_layout;
-            pipeline_desc.raster_state_desc.cull_mode = CullMode::BACK_CCW;
-            pipeline_desc.depth_stencil_state.depth_write = FALSE;
-            pipeline_desc.used_stages = PIPELINE_STAGE_VERTEX | PIPELINE_STAGE_PIXEL;
+            // Compile the shader
+            rhi::ShaderCompilationDesc shader_compilation_desc = {
+                .used_stages = rhi::PIPELINE_STAGE_VERTEX | rhi::PIPELINE_STAGE_PIXEL,
+                .shader_file_path = L"shaders/normal_mapping.hlsl",
+            };
 
-            pso_handle = gfx::pipelines::create_pipeline_state_object(pipeline_desc);
+            std::string errors{};
+            rhi::ShaderBlobsHandle blobs_handle{};
+            ZecResult res = renderer.shaders_compile(shader_compilation_desc, blobs_handle, errors);
+            if (res != ZecResult::SUCCESS || !errors.empty())
+            {
+                OutputDebugStringA(errors.c_str());
+                ASSERT_FAIL("Shader compilation failed");
+            }
+
+            rhi::PipelineStateObjectDesc pipeline_desc = {};
+            pipeline_desc.input_assembly_desc = { {
+                { rhi::MeshAttribute::POSITION, 0, rhi::BufferFormat::FLOAT_3, 0 },
+                { rhi::MeshAttribute::NORMAL, 0, rhi::BufferFormat::FLOAT_3, 1 },
+                { rhi::MeshAttribute::TEXCOORD, 0, rhi::BufferFormat::FLOAT_2, 2 },
+            } };
+            pipeline_desc.rtv_formats[0] = rhi::BufferFormat::R8G8B8A8_UNORM_SRGB;
+            pipeline_desc.raster_state_desc.cull_mode = rhi::CullMode::BACK_CCW;
+            pipeline_desc.depth_stencil_state.depth_write = FALSE;
+
+            pso_handle = renderer.pipelines_create(blobs_handle, resource_layout, pipeline_desc);
+            renderer.shaders_release_blobs(blobs_handle);
         }
 
-        gfx::begin_upload();
-
+        rhi::CommandContextHandle cmd_ctx = renderer.cmd_provision(rhi::CommandQueueType::COPY);
         // Create the vertex buffer.
         {
             // Define the geometry for a cube.
@@ -203,45 +205,53 @@ protected:
                 23, 22, 20,
             };
 
-            MeshDesc mesh_desc{};
-            mesh_desc.index_buffer_desc.usage = RESOURCE_USAGE_INDEX;
-            mesh_desc.index_buffer_desc.type = BufferType::DEFAULT;
-            mesh_desc.index_buffer_desc.byte_size = sizeof(cube_indices);
-            mesh_desc.index_buffer_desc.stride = sizeof(cube_indices[0]);
-            mesh_desc.index_buffer_desc.data = (void*)cube_indices;
+            rhi::BufferDesc index_buffer_desc = {
+                .usage = rhi::RESOURCE_USAGE_INDEX,
+                .type = rhi::BufferType::DEFAULT,
+                .byte_size = sizeof(cube_indices),
+                .stride = sizeof(cube_indices[0]),
+            };
+            cube_mesh.index_buffer = renderer.buffers_create(index_buffer_desc);
+            cube_mesh.index_count = ARRAYSIZE(cube_indices);
+            renderer.buffers_set_data(cmd_ctx, cube_mesh.index_buffer, cube_indices, sizeof(cube_indices));
 
-            mesh_desc.vertex_buffer_descs[0] = {
-                    .usage = RESOURCE_USAGE_VERTEX,
-                    .type = BufferType::DEFAULT,
+            rhi::BufferDesc vertex_buffer_desc = {
+                    .usage = rhi::RESOURCE_USAGE_VERTEX,
+                    .type = rhi::BufferType::DEFAULT,
                     .byte_size = sizeof(cube_positions),
                     .stride = 3 * sizeof(cube_positions[0]),
-                    .data = (void*)(cube_positions)
             };
-            mesh_desc.vertex_buffer_descs[1] = {
-               .usage = RESOURCE_USAGE_VERTEX,
-               .type = BufferType::DEFAULT,
+            cube_mesh.vertex_buffers[cube_mesh.num_vertex_buffers] = renderer.buffers_create(vertex_buffer_desc);
+            renderer.buffers_set_data(cmd_ctx, cube_mesh.vertex_buffers[cube_mesh.num_vertex_buffers++], cube_positions, sizeof(cube_positions));
+
+            vertex_buffer_desc = {
+                .usage = rhi::RESOURCE_USAGE_VERTEX,
+                .type = rhi::BufferType::DEFAULT,
                .byte_size = sizeof(cube_normals),
                .stride = 3 * sizeof(cube_normals[0]),
-               .data = (void*)(cube_normals)
             };
-            mesh_desc.vertex_buffer_descs[2] = {
-               .usage = RESOURCE_USAGE_VERTEX,
-               .type = BufferType::DEFAULT,
+            cube_mesh.vertex_buffers[cube_mesh.num_vertex_buffers] = renderer.buffers_create(vertex_buffer_desc);
+            renderer.buffers_set_data(cmd_ctx, cube_mesh.vertex_buffers[cube_mesh.num_vertex_buffers++], cube_normals, sizeof(cube_normals));
+            vertex_buffer_desc = {
+                .usage = rhi::RESOURCE_USAGE_VERTEX,
+                .type = rhi::BufferType::DEFAULT,
                .byte_size = sizeof(cube_uvs),
                .stride = 2 * sizeof(cube_uvs[0]),
-               .data = (void*)(cube_uvs)
             };
-
-            cube_mesh = gfx::meshes::create(mesh_desc);
+            cube_mesh.vertex_buffers[cube_mesh.num_vertex_buffers] = renderer.buffers_create(vertex_buffer_desc);
+            renderer.buffers_set_data(cmd_ctx, cube_mesh.vertex_buffers[cube_mesh.num_vertex_buffers++], cube_uvs, sizeof(cube_uvs));
         }
 
         // Texture creation
         {
-            albedo_map = gfx::textures::load_from_file("textures/stone01.dds");
-            normal_map = gfx::textures::load_from_file("textures/bump01.dds");
+            albedo_map = renderer.textures_create_from_file(cmd_ctx, L"textures/stone01.dds");
+            normal_map = renderer.textures_create_from_file(cmd_ctx, L"textures/bump01.dds");
+
+            draw_constant_data.albedo_map_idx = renderer.get_readable_index(albedo_map);
+            draw_constant_data.normal_map_idx = renderer.get_readable_index(normal_map);
         }
 
-        gfx::end_upload();
+        rhi::CmdReceipt receipt = renderer.cmd_return_and_execute(&cmd_ctx, 1);
 
         camera.projection = perspective_projection(
             float(width) / float(height),
@@ -252,19 +262,20 @@ protected:
 
         // Create constant buffers
         {
-            BufferDesc cb_desc = {
-                .usage = RESOURCE_USAGE_CONSTANT | RESOURCE_USAGE_DYNAMIC,
-                .type = BufferType::DEFAULT,
+            rhi::BufferDesc cb_desc = {
+                .usage = rhi::RESOURCE_USAGE_CONSTANT | rhi::RESOURCE_USAGE_DYNAMIC,
+                .type = rhi::BufferType::DEFAULT,
                 .byte_size = sizeof(ViewConstantData),
                 .stride = 0,
-                .data = nullptr,
             };
 
-            view_cb_handle = gfx::buffers::create(cb_desc);
+            view_cb_handle = renderer.buffers_create(cb_desc);
 
             // Same size and usage etc, so we can use the same desc
-            draw_cb_handle = gfx::buffers::create(cb_desc);
+            draw_cb_handle = renderer.buffers_create(cb_desc);
         }
+
+        renderer.cmd_cpu_wait(receipt);
     }
 
     void shutdown() override final
@@ -272,7 +283,13 @@ protected:
 
     void update(const zec::TimeData& time_data) override final
     {
-        camera_controller.update(time_data.delta_seconds_f);
+        const input::InputState input_state = input_manager.get_state();
+        camera_controller.update(camera, input_state, time_data.delta_seconds_f);
+        camera_controller.apply(camera);
+    }
+
+    void copy(const zec::TimeData& time_data) override final
+    {
         view_constant_data.view = camera.view;
         view_constant_data.projection = camera.projection;
         view_constant_data.VP = camera.projection * camera.view;
@@ -281,41 +298,32 @@ protected:
 
         draw_constant_data.model = identity_mat4();
         draw_constant_data.inv_model = identity_mat4();
-        // TODO: Actually grab the relevant descriptors?
-        draw_constant_data.albedo_map_idx = gfx::textures::get_shader_readable_index(albedo_map);
-        draw_constant_data.normal_map_idx = gfx::textures::get_shader_readable_index(normal_map);
     }
 
-    void copy() override final
+    void render(const zec::TimeData& time_data) override final
     {
-        gfx::buffers::update(view_cb_handle, &view_constant_data, sizeof(view_constant_data));
-        gfx::buffers::update(draw_cb_handle, &draw_constant_data, sizeof(draw_constant_data));
-    }
+        renderer.buffers_update(view_cb_handle, &view_constant_data, sizeof(view_constant_data));
+        renderer.buffers_update(draw_cb_handle, &draw_constant_data, sizeof(draw_constant_data));
 
-    void render() override final
-    {
-        CommandContextHandle cmd_ctx = gfx::begin_frame();
+        rhi::CommandContextHandle cmd_ctx = renderer.begin_frame();
+        rhi::Viewport viewport = { 0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height) };
+        rhi::Scissor scissor{ 0, 0, width, height };
 
-        Viewport viewport = { 0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height) };
-        Scissor scissor{ 0, 0, width, height };
+        rhi::TextureHandle backbuffer = renderer.get_current_back_buffer_handle();
+        renderer.cmd_clear_render_target(cmd_ctx, backbuffer, clear_color);
 
-        TextureHandle backbuffer = gfx::get_current_back_buffer_handle();
-        gfx::cmd::clear_render_target(cmd_ctx, backbuffer, clear_color);
+        renderer.cmd_set_graphics_resource_layout(cmd_ctx, resource_layout);
+        renderer.cmd_set_graphics_pipeline_state(cmd_ctx, pso_handle);
+        renderer.cmd_bind_graphics_constant_buffer(cmd_ctx, draw_cb_handle, 0);
+        renderer.cmd_bind_graphics_constant_buffer(cmd_ctx, view_cb_handle, 1);
 
-        gfx::cmd::set_active_resource_layout(cmd_ctx, resource_layout);
-        gfx::cmd::set_pipeline_state(cmd_ctx, pso_handle);
-        gfx::cmd::bind_constant_buffer(cmd_ctx, draw_cb_handle, 0);
-        gfx::cmd::bind_constant_buffer(cmd_ctx, view_cb_handle, 1);
-        gfx::cmd::bind_resource_table(cmd_ctx, 2);
+        renderer.cmd_set_viewports(cmd_ctx, &viewport, 1);
+        renderer.cmd_set_scissors(cmd_ctx, &scissor, 1);
 
+        renderer.cmd_set_render_targets(cmd_ctx, &backbuffer, 1);
+        renderer.cmd_draw(cmd_ctx, cube_mesh);
 
-        gfx::cmd::set_viewports(cmd_ctx, &viewport, 1);
-        gfx::cmd::set_scissors(cmd_ctx, &scissor, 1);
-
-        gfx::cmd::set_render_targets(cmd_ctx, &backbuffer, 1);
-        gfx::cmd::draw_mesh(cmd_ctx, cube_mesh);
-
-        gfx::end_frame(cmd_ctx);
+        renderer.end_frame(cmd_ctx);
     }
 
     void before_reset() override final
--- runtime/shaders/normal_mapping.hlsl
+++ runtime/shaders/normal_mapping.hlsl
@@ -1,14 +1,5 @@
-//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
 #pragma pack_matrix( row_major )
+#include "common.hsh"
 
 cbuffer draw_constants_buffer : register(b0)
 {
@@ -27,10 +18,6 @@ cbuffer view_constants_buffer : register(b1)
     float time;
 };
 
-SamplerState default_sampler : register(s0);
-
-Texture2D tex2D_table[4096] : register(t0, space1);
-
 struct PSInput
 {
     float4 position_cs : SV_POSITION;
@@ -38,6 +25,8 @@ struct PSInput
     float3 normal_ws : NORMAL;
     float2 uv : TEXCOORD;
 };
+
+static SamplerState default_sampler;
 
 //=================================================================================================
 // Helper Functions
@@ -71,11 +60,6 @@ float3 perturb_normal(Texture2D normal_map, float3 N, float3 V, float2 texcoord)
     return normalize(mul(map, TBN));
 }
 
-float clamp_dot(float3 A, float3 B)
-{
-    return clamp(dot(A, B), 0.0, 1.0f);
-}
-
 //=================================================================================================
 // Vertex Shader
 //=================================================================================================
@@ -98,15 +82,16 @@ PSInput VSMain(float3 position : POSITION, float3 normal : NORMAL, float2 uv : T
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-    Texture2D albedo_texture = tex2D_table[albedo_texture_idx];
-    Texture2D normal_texture = tex2D_table[normal_texture_idx];
+    Texture2D albedo_texture = ResourceDescriptorHeap[albedo_texture_idx];
+    Texture2D normal_texture = ResourceDescriptorHeap[normal_texture_idx];
 
     float3 view_dir = camera_pos - input.position_ws.xyz;
 
     const float3 light_pos = float3(5.0 * sin(time), 6.0, 5.0 * cos(time));
     const float light_intensity = 30.0f;
     const float3 light_color = float3(1.0, 1.0, 1.0);
-
+    
+    default_sampler = SamplerDescriptorHeap[SamplersIndices::ANISOTROPIC_WRAP];
     float3 base_color = albedo_texture.Sample(default_sampler, input.uv).rgb;
     float3 normal = perturb_normal(normal_texture, input.normal_ws, view_dir, input.uv);
 
